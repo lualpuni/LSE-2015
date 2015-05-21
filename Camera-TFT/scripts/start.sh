b47c41a6bd06d48028c991ca36d60fe3734f678f
pcholbi <p.cholbi@alumnos.upm.es>
2015-05-21 12:50:03 +0000

    [camctrl] debbuged

--- Camera-TFT/scripts/start.sh
+++ Camera-TFT/scripts/start.sh
@@ -2,6 +2,6 @@
 
 sudo /etc/init.d/fbcp start
 sudo /etc/init.d/servoctrl start
-screen -d -m -S camctrl python /usr/local/bin/camctrl.py
+screen -d -m -S camctrl sudo python /usr/local/bin/camctrl.py
 
 exit 1
--- Camera-TFT/scripts/test/camctrl_test.sh
+++ Camera-TFT/scripts/test/camctrl_test.sh
@@ -5,21 +5,11 @@ shdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 sudo /etc/init.d/fbcp start
 sudo /etc/init.d/servoctrl start
-screen -d -m -S camctrl python /usr/local/bin/camctrl.py
+screen -d -m -S camctrl sudo python /usr/local/bin/camctrl.py
 
 echo "Is the TFT LCD touchscreen relaying the camera image [Y/n]?"
 read ans1
 if ! [ $ans1 = "y" -o $ans1 = "Y" -o $ans1 = "yes" -o $ans1 = "YES" ]
-then
-	exit 1
-fi
-
-# Send "pause" command
-python $shdir/camctrl-dummy.py 0 0 0 0
-
-echo "Has the TFT LCD touchscreen stopped relaying the camera image [Y/n]?"
-read ans2
-if ! [ $ans2 = "y" -o $ans2 = "Y" -o $ans2 = "yes" -o $ans2 = "YES" ]
 then
 	exit 1
 fi
@@ -37,8 +27,8 @@ for i in `seq 1 5`;  do python $shdir/camctrl-dummy.py 1 1 0 1; done
 for i in `seq 1 5`;  do python $shdir/camctrl-dummy.py 1 0 1 1; done
 
 echo "Did the mechanism follow a square path [Y/n]?"
-read ans3
-if ! [ $ans3 = "y" -o $ans3 = "Y" -o $ans3 = "yes" -o $ans3 = "YES" ]
+read ans2
+if ! [ $ans2 = "y" -o $ans2 = "Y" -o $ans2 = "yes" -o $ans2 = "YES" ]
 then
 	exit 1
 fi
